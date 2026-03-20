83f812794c75a5ebdea28b51037f16e508083e8f
Renu2007 <renunatekar2007@gmail.com>
2026-03-20 19:37:35 +0530

    Implement basic arithmetic operations in Python

---
+++ basic_operation.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+sum=a+b
+diffrence=a-b
+product=a*b
+quotient=a/b
+remainder=a%b
+print("Addtion :",sum)
+print("Subtraction :",diffrence)
+print("Multiplication :",product)
+print("Division :",quotient)
+print("Modulus :",product)
